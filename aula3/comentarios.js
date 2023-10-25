626f9c84f6492a3070fc00062c19a8b76fdc5df1
Yann Szilagyi <109697467+yannfgs@users.noreply.github.com>
2023-10-25 15:30:27 -0300

    25102301u

---
+++ aula3/comentarios.js
@@ -0,0 +1,22 @@
+// E escrever um comentário
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// Comentário
+
+/*
+Um bloco de código
+Linha d comentário.
+*/
+
+// console.log('Linha 2');
+
+/**
+ * Outro bloco de código
+ * Um título
+ * Etc
+ * Lorem ipsum
+ */
+
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
